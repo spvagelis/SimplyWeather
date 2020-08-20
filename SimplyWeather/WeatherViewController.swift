764ac0a4487ec0902479c899e13f6417b663c920
spvagelis <spirou.vagelis@gmail.com>
2020-08-20 12:42:24 +0300

    Complete the UI of the App.

---
+++ SimplyWeather/WeatherViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WeatherViewController.swift
+//  SimplyWeather
+//
+//  Created by vagelis spirou on 19/8/20.
+//  Copyright © 2020 vagelis spirou. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentConditionWeatherLabel: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
